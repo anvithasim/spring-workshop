8dc69a573f2e3075137e33c4119c730a8a0cbc61
anvithasim <simhadri.anvitha@gmail.com>
2025-04-09 20:50:39 -0500

    workshopfunfact

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function anvithaFunFact() {
+    const participants = [
+        {
+            name: "Anvitha",
+            funFact: "I love creativity and design!"
+        },
+    ];
+}
+
+
+anvithaFunFact();
